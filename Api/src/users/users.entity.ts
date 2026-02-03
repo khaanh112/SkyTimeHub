ee27725885aa0200d735aff381a018236ed3a62b
kharanh112 <maikhaanh11205@gmail.com>
2026-02-03 09:22:01 +0700

    init base for project

---
+++ Api/src/users/users.entity.ts
@@ -0,0 +1,64 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, ManyToOne, JoinColumn } from "typeorm";
+import { UserRole } from "../common/enums/roles.enum";
+import { UserStatus } from "src/common/enums/user-status.enum";
+
+
+@Entity('users')
+export class User {
+  
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'employee_id', unique: true, nullable: true })
+  employeeId: string;
+
+  @Column()
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+
+  @Column({ name: 'refresh_token_hash', nullable: true })
+  refreshTokenHash: string;
+
+  @Column({
+    type: "enum",
+    enum: UserRole,
+    default: UserRole.EMPLOYEE,
+  })
+  role: UserRole;
+
+  @Column({
+    type: "enum",
+    enum: UserStatus,
+    default: UserStatus.INACTIVE,
+  })
+  status: UserStatus; 
+
+  @Column({ name: 'department_id', nullable: true })
+  departmentId: number;
+
+
+  @Column({ type: 'varchar', length: 100, nullable: true })
+  position: string;
+
+  @Column({ type: 'varchar', length: 20, nullable: true })
+  phone: string;
+
+  @Column({ name: 'join_date', type: 'date', nullable: true })
+  joinDate: Date;
+
+  @Column({ name: 'activation_token', nullable: true })
+  activationToken: string;
+
+  @Column({ name: 'activated_at', type: 'timestamp', nullable: true })
+  activatedAt: Date;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+}
