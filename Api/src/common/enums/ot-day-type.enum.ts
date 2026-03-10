c1a12624edf4c84c62ba0910ee2df2e53b2eea34
kharanh112 <maikhaanh11205@gmail.com>
2026-03-10 15:19:22 +0700

    ot-day-type enum

--- Api/src/common/enums/ot-day-type.enum.ts
+++ Api/src/common/enums/ot-day-type.enum.ts
@@ -3,4 +3,7 @@ export enum OtDayType {
   WEEKEND = 'weekend',
   HOLIDAY = 'holiday',
   WEEKDAY_NIGHT_NO_DAY_OT = 'weekday_night_no_day_ot',
+  WEEKDAY_NIGHT_WITH_DAY_OT = 'weekday_night_with_day_ot',
+  WEEKEND_NIGHT = 'weekend_night',
+  HOLIDAY_NIGHT = 'holiday_night',
 }
